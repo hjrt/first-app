57dc1708426e91b6c30b6b5a369842b2a3d87371
aleksandra <kjerte@gmail.com>
2016-08-12 21:00:13 +0200

    dodane rzecz

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :answers
-  resources :questions
+  #resources :answers
+  resources :questions do 
+  	resources :answers
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
