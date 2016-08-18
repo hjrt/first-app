99f7be245fc712f2c220ec7ee5a33f5c7c91b6fe
Alex K <kjerte@gmail.com>
2016-08-18 19:45:30 +0200

    naprawa nazwy pól question

--- Gemfile
+++ Gemfile
@@ -33,6 +33,10 @@ gem 'jbuilder', '~> 2.5'
 gem 'devise'
 
 group :development, :test do
+  gem 'pry'
+  gem 'pry-rails'
+  gem 'better_errors'
+  gem 'binding_of_caller'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,8 @@ ActiveRecord::Schema.define(version: 20160812184746) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string   "name"
+    t.string   "title"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
