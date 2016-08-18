96a8ec420c3eedec6d31ddfd31702361ed713012
Alex K <kjerte@gmail.com>
2016-08-18 20:58:25 +0200

    ciąg dalszy devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,31 @@ class User < ApplicationRecord
   
   # Virtual attribute for authenticating by either username or email
   # This is in addition to a real persisted field like 'username'
-  attr_accessor :login
+  # attr_accessor :login
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.username || self.email
+  end
+
+    def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      if login = conditions.delete(:login)
+        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+        where(conditions.to_h).first
+      end
+    end
+
+  validates :username,
+  :presence => true,
+  :uniqueness => {
+    :case_sensitive => false
+  } # etc.
+
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
 end
