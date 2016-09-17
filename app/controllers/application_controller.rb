d50110044abee89471f96fc597cb28b06042b090
Natalia Stanko <anithaly@gmail.com>
2016-09-17 19:18:08 +0200

    rm commented code in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,6 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
-  #   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :password, :remember_me) }
-  #   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :avatar, :avatar_cache, :remove_avatar) }
-  # end
-
   def configure_permitted_parameters
     # added_attrs = [:username, :email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache, :remove_avatar]
     devise_parameter_sanitizer.permit :sign_up, keys: [:username, :email, :password, :password_confirmation]
