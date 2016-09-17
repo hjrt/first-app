bc9178b06c946a4f58841d1ebbec6c0b53da127c
Alex K <kjerte@gmail.com>
2016-09-17 15:44:00 +0200

    carrierwave

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -25,7 +25,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-  process :scale => [100, 100]
+  #process :scale => [100, 100]
   #
   # def scale(width, height)
   #   # do something
---
+++ db/seeds.rb
@@ -0,0 +1,1 @@
+ u = User.create({username: 'test1', email: 'example1@example.com', password: '123456', password_confirmation: '123456'})
