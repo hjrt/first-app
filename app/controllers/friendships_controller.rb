61af519097c01ba7047ebe915033e21a41835dd5
Alex K <kjerte@gmail.com>
2016-10-21 15:43:31 +0200

    added create and destroy methods in friendships controller and changed iteration through friends in user views

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,20 @@
 class FriendshipsController < ApplicationController
+
+  def create
+  @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+  if @friendship.save
+    flash[:notice] = "Friend request has been sent."
+    redirect_back(fallback_location: root_path)
+  else
+    flash[:notice] = "Unable to sent friend request."
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @friendship = current_user.friendships.find(params[:id])
+    @friendship.destroy
+    flash[:notice] = "Unfriended."
+    redirect_back(fallback_location: root_path)
+  end
+
 end
